d1bb8da2201f27e2dd997e0f2dcee60a8d46f557
pradeeppathak9 <pradeeppathak9@gmail.com>
2020-01-23 15:57:48 +0530

    Create logger.py

---
+++ logger.py
@@ -0,0 +1,32 @@
+import requests
+
+webhook_url = "slack-webhook-url"
+
+class SlackWebhook():
+    def __init__(self, webhook_url, verbose=False):
+        self.webhook_url = webhook_url
+        self.verbose = verbose
+    
+    def __call__(self, *args, **kwargs):
+        message, sep = "", " - "
+        for a in args: 
+            message += "{}".format(a) + sep
+        for key, val in kwargs.items(): 
+            message += "{}: {}".format(key, val) + sep 
+        message = message.strip(sep)
+        self.send(message)
+    
+    def log(self, message):
+        if self.verbose: print("{}".format(message))
+            
+    def send(self, message):
+        try:
+            res = requests.post(url=self.webhook_url, headers={"Content-type": "application/json"}, json={"text":message})
+            self.log(res.status_code)
+        except Exception as e:
+            self.log(e)
+        
+        
+slack_loggger = SlackWebhook(webhook_url)
+slack_loggger("New Model",  epoch=1, val_score=1.23)
+    
